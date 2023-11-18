fffa2dceea7a2e39ff7783b6024029a5093dde73
AryamMehari <aryam@hotmail.co.uk>
2023-11-18 18:13:00 +0000

    The PortfolioService class has been updated to include a method which adds a new portfolio to the existing list of portfolios. The ApiController has been updated to include a POST request which allows a user to input a new portfolio and returns a reponse based on whether the POST has been successful or not.

--- src/main/java/com/cbfacademy/apiassessment/Controller/ApiController.java
+++ src/main/java/com/cbfacademy/apiassessment/Controller/ApiController.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.annotation.ComponentScan;
@@ -31,22 +33,24 @@ public class ApiController {
 
         private final PortfolioService portfolioService;
 
-        //Injecting a PortfolioService instance into the controller
+        // Injecting a PortfolioService instance into the controller
         @Autowired
         public PortfolioController(PortfolioService portfolioService) {
             this.portfolioService = portfolioService;
         }
-        
-        //This code represents an endpoint which returns a list of portfolios using the getAllPortfolios method
+
+        // This code represents an endpoint which returns a list of portfolios using the
+        // getAllPortfolios method
         @GetMapping("/api/portfolios")
         public List<PortfolioData.Portfolio> getAllPortfolios() {
             return portfolioService.getAllPortfolios();
         }
 
-         // This endpoint returns a portfolio by the specified ID in the URL
+        // This endpoint returns a portfolio by the specified ID in the URL
         @GetMapping("/api/portfolios/{id}")
         public ResponseEntity<PortfolioData.Portfolio> getPortfolioById(@PathVariable int id) {
-            //The getPortfoliobyId method is called to retrieve a portfolio by a specified id; an error message is returned if the portfolio is not found
+            // The getPortfoliobyId method is called to retrieve a portfolio by a specified
+            // id; an error message is returned if the portfolio is not found
             PortfolioData.Portfolio portfolio = portfolioService.getPortfolioById(id);
 
             if (portfolio != null) {
@@ -56,6 +60,22 @@ public class ApiController {
             }
         }
 
+        // This endpoint handles a post request to create a new portfolio, it expects a
+        // portfolio which follows the structure specified in the PortfolioData class.
+        // This method returns a response based on the success or failure of adding a
+        // new portfolio
+        @PostMapping("/api/new/portfolio")
+        public ResponseEntity<String> addPortfolio(@RequestBody PortfolioData.Portfolio newPortfolio) {
+            try {
+                portfolioService.addPortfolio(newPortfolio);
+                return new ResponseEntity<>("Portfolio added successfully", HttpStatus.CREATED);
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+                return new ResponseEntity<>("Error adding portfolio: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+            }
+
+        }
+
     }
-    
+
 }
--- src/main/java/com/cbfacademy/apiassessment/service/PortfolioService.java
+++ src/main/java/com/cbfacademy/apiassessment/service/PortfolioService.java
@@ -8,6 +8,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.lang.reflect.Type;
 import java.util.List;
 
@@ -36,13 +37,46 @@ public class PortfolioService {
             return null;
         }
     }
+
     // This method takes an id parameter and returns the corresponding portfolio
     public Portfolio getPortfolioById(int id) {
-        //Converting list into a stream and filters to find the portfolio with a specific id and takes the first instance of the specific id
+       
+        // Converting list into a stream and filters to find the portfolio with a specific id and takes the first instance of the specific id
         List<PortfolioData.Portfolio> portfolios = getAllPortfolios();
         return portfolios.stream()
                 .filter(portfolio -> portfolio.getId() == id)
                 .findFirst()
                 .orElse(null);
     }
+
+    // This method adds a new portfolio to the existing list of portfolios
+    public void addPortfolio(PortfolioData.Portfolio newPortfolio) {
+        try {
+            // Define the path to the JSON file
+            String jsonFilePath = "/Users/aryammehari/java-rest-api-assessment-AryamMehari/src/main/java/com/cbfacademy/apiassessment/DataStorage/portfolioData.json";
+
+            // Create a Gson instance
+            Gson gson = new Gson();
+
+            // Read existing JSON data from the file
+            Type listType = new TypeToken<List<PortfolioData.Portfolio>>() {
+            }.getType();
+            List<PortfolioData.Portfolio> existingPortfolios = gson.fromJson(new FileReader(jsonFilePath), listType);
+
+            // Add the new portfolio to the list
+            existingPortfolios.add(newPortfolio);
+
+            // Write the updated list back to the file
+            try (FileWriter writer = new FileWriter(jsonFilePath)) {
+                gson.toJson(existingPortfolios, writer);
+            }
+
+        } catch (Exception e) {
+            // Handle exceptions, such as file not found
+            e.printStackTrace();
+            System.err.println("Error adding portfolio: " + e.getMessage());
+        }
+
+    }
+
 }
